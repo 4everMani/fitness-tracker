4adaad6371577d841a6586a5633a93e768c0d6ae
Manish Jaiswal <jaiswal.manish49@gmail.com>
2022-06-01 07:16:07 +0530

    initial signup from creation and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LoginComponent } from './auth/login/login.component';
 import { MaterialModeule } from './material.module';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { NgModule } from '@angular/core';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModeule,
+    FormsModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
