3833ff1bfddaa51ebbbba50fb52007c09de1f233
Manish Jaiswal <jaiswal.manish49@gmail.com>
2022-06-01 05:45:39 +0530

    project setup completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModeule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModeule,
   ],
   providers: [],
   bootstrap: [AppComponent]
